e1f109666b9736c298f7607f4eae1d5ca1ee2849
margaret manyeki <maggiemanyeki6@gmail.com>
2022-07-21 14:09:18 +0300

    Done.

--- src/components/ThemedButton.js
+++ src/components/ThemedButton.js
@@ -1,10 +1,9 @@
-import React from "react";
-import {ThemeContext} from "../context/theme";
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/theme";
 
 function ThemedButton({ ...props }) {
   const { theme } = useContext(ThemeContext);
   return <button className={theme} {...props} />;
 }
-
 
 export default ThemedButton;
